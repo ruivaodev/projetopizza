19cbee8c34bb88421c8fbc48eaea2bd5037e8186
schirmann <cschirmann.dev@gmail.com>
2021-03-24 15:10:12 -0300

    itens adicionados img price e description

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-//não é inteligente usar isso, estou usando por ser a pratica do professor.
+// clone dos itens que estão no pizza.js atraves das divs
 
 pizzaJson.map((item, index) => {
   let pizzaItem = document.querySelector(".models .pizza-item").cloneNode(true);
 
+  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+
+  pizzaItem.querySelector(
+    ".pizza-item--price"
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`;
+
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
 
   document.querySelector(".pizza-area").append(pizzaItem);
 });
