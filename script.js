2a851aa2191fe0514d09f2c6c4c3cd1bad7e2589
schirmann <cschirmann.dev@gmail.com>
2021-03-24 19:59:22 -0300

    adicionando atividades ao modal

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 pizzaJson.map((item, index) => {
   let pizzaItem = document.querySelector(".models .pizza-item").cloneNode(true);
 
+  pizzaItem.setAttribute("data-key", index);
+
   pizzaItem.querySelector(".pizza-item--img img").src = item.img;
 
   pizzaItem.querySelector(
@@ -15,6 +17,10 @@ pizzaJson.map((item, index) => {
 
   pizzaItem.querySelector("a").addEventListener("click", (e) => {
     e.preventDefault();
+
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+
+    document.querySelector(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
 
     document.querySelector(".pizzaWindowArea").style.opacity = 0;
     document.querySelector(".pizzaWindowArea").style.display = "flex";
