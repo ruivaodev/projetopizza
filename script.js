7f45db083254edef350a73adec40eb9f71c0850c
schirmann <cschirmann.dev@gmail.com>
2021-03-24 23:35:10 -0300

    add eventos aos botoes

--- script.js
+++ script.js
@@ -65,3 +65,27 @@ document
   .forEach((item) => {
     item.addEventListener("click", closeModal);
   });
+
+// adicionando ações aos botoes de + e -
+
+document.querySelector(".pizzaInfo--qtmenos").addEventListener("click", () => {
+  if (modalQt > 1) {
+    modalQt--;
+    document.querySelector(".pizzaInfo--qt").innerHTML = modalQt;
+  }
+});
+
+document.querySelector(".pizzaInfo--qtmais").addEventListener("click", () => {
+  modalQt++;
+  document.querySelector(".pizzaInfo--qt").innerHTML = modalQt;
+});
+
+//adicionando a função selected aos botoes dos tamanhos
+document.querySelectorAll(".pizzaInfo--size").forEach((size, sizeIndex) => {
+  size.addEventListener("click", (e) => {
+    document
+      .querySelector(".pizzaInfo--size.selected")
+      .classList.remove("selected");
+    size.classList.add("selected");
+  });
+});
